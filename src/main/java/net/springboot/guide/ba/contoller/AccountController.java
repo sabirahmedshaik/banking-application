43a6d8c2b1364053488a12abe7c2549daaf6288e
sabirahmedshaik <sabirahmed.piya@gmail.com>
2024-05-11 13:32:13 +0530

    Create AccountController

---
+++ src/main/java/net/springboot/guide/ba/contoller/AccountController.java
@@ -0,0 +1,14 @@
+package net.springboot.guide.ba.contoller;
+
+import net.springboot.guide.ba.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+}

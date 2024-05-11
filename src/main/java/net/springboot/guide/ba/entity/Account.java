2c7c8b9b068ba2d08288ac9d71d1f7f14788ab55
sabirahmedshaik <sabirahmed.piya@gmail.com>
2024-05-11 12:21:01 +0530

    Create Account Entity

---
+++ src/main/java/net/springboot/guide/ba/entity/Account.java
@@ -0,0 +1,26 @@
+package net.springboot.guide.ba.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "holder_name")
+    private String accountholderName;
+
+    @Column(name = "balance")
+    private double balance;
+}
